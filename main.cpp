016fb12e368f8a64f6b19e5d0cb7804998cc87da
duminoah <duminoah9711@gmail.com>
2020-07-16 10:40:11 +0100

    On branch master

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main(void)
 {
- 	cout<<"Hello to My World @ Terminal Win" <<endl ; 
-
+ 	cout<<"Hello to My World @ Terminal Win" <<endl ;
+        cout<<"Enter a any number of your choice" <<endl ; 
+        int num;
+        cin>> num ;
+        cout<<"Thats a good number"<<num << endl ;
  return 0;
 }
